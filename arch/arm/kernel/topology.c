c5e2fd42c4d81f63c8f4b2edf8d86c2e5d6295d4
Pranav Vashi <neobuddy89@gmail.com>
2014-06-29 16:34:29 +0530

    sched: cpu_power: Add controls for sched features
    * Currently we provide users attributes to enable/
      disable GENTLE_FAIR_SLEEPERS and ARCH_POWER
    * This patch uses already implemented GFS structure
      to add ARCH_POWER attribute.
    * Attributes can be found under /sys/kernel/sched/*
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    
    	kernel/ksysfs.c
    	kernel/sched/fair.c

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -258,11 +258,8 @@ void store_cpu_topology(unsigned int cpuid)
 	}
 
 	update_siblings_masks(cpuid);
-<<<<<<< HEAD
 
 	update_cpu_power(cpuid);
-=======
->>>>>>> 15336eb... ARM: 7462/1: topology: factorize the update of sibling masks
 
 	printk(KERN_INFO "CPU%u: thread %d, cpu %d, socket %d, mpidr %x\n",
 		cpuid, cpu_topology[cpuid].thread_id,
