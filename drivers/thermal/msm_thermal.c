375427d449d0bdc9c7ffd37e36dfbc03037b1eb1
Andy1911 <aanand.godbole17@gmail.com>
2015-12-17 19:40:50 +0530

    Revert "Thermal Changes"
    This reverts commit 3dc21fabf5474f55f8f16d966e43b8745b5bde7f.

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -1565,6 +1565,19 @@ static __ref int do_freq_mitigation(void *data)
 			;
 		INIT_COMPLETION(freq_mitigation_complete);
 
+<<<<<<< HEAD
+		ret = therm_get_temp(msm_thermal_info.sensor_id,
+			THERM_TSENS_ID, &temp);
+		if (ret)
+			pr_err("Unable to read TSENS sensor:%d\n",
+				msm_thermal_info.sensor_id);
+		else if (temp <= msm_thermal_info.limit_temp_degC)
+			skip_mitig = true;
+		else
+			skip_mitig = false;
+
+=======
+>>>>>>> 3762775... msm: thermal: Optimize locking in thermal frequency mitigation thread
 		for_each_possible_cpu(cpu) {
 			max_freq_req = (cpus[cpu].max_freq) ?
 					msm_thermal_info.freq_limit :
